07d98c9d749594faf402230c20233686c7c5d09f
Baumbart13 <thomaskefer0109@gmail.com>
2021-06-16 20:10:58 +0200

    Added some print-methods.

--- src/at/htlAnich/tools/BaumbartLogger.java
+++ src/at/htlAnich/tools/BaumbartLogger.java
@@ -14,4 +14,64 @@ public final class BaumbartLogger {
 		errf(s.concat("%n"), o);
 	}
 	public static void waitForKeyPress(){var s = new java.util.Scanner(System.in); s.nextLine();}
+	public static void log(String s){
+		logf(s);
+	}
+	public static void log(Object s){
+		log(s.toString());
+	}
+	public static void log(byte s){
+		log(s);
+	}
+	public static void log(char s){
+		log(s);
+	}
+	public static void log(short s){
+		log(s);
+	}
+	public static void log(int s){
+		log(s);
+	}
+	public static void log(long s){
+		log(s);
+	}
+	public static void log(float s){
+		log(s);
+	}
+	public static void log(double s){
+		log(s);
+	}
+	public static void log(boolean s){
+		log(s);
+	}
+	public static void logln(String s){
+		log(s+System.lineSeparator());
+	}
+	public static void logln(Object s){
+		logln(s.toString());
+	}
+	public static void logln(byte s){
+		logln(s);
+	}
+	public static void logln(char s){
+		logln(s);
+	}
+	public static void logln(short s){
+		logln(s);
+	}
+	public static void logln(int s){
+		logln(s);
+	}
+	public static void logln(long s){
+		logln(s);
+	}
+	public static void logln(float s){
+		logln(s);
+	}
+	public static void logln(double s){
+		logln(s);
+	}
+	public static void logln(boolean s){
+		logln(s);
+	}
 }
